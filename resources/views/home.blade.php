7d1af5c0785be06f0541d1a149b5c3751105c8b4
mjremetio <mj.remetio001@gmail.com>
2020-05-07 13:48:06 +0800

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-19/s150x150/57348348_279964586285572_6163317076652982272_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_ohc=Sw597m3fxiQAX9a6sup&oh=6953db302404296d19db7db73137ae52&oe=5EDBC8E3" alt="no image detected" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="div"><h1 class="h1">Mark Joseph Remetio</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>50</strong> posts</div>
+                <div class="pr-5"><strong>1k</strong> followers</div>
+                <div class="pr-5"><strong>3k</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">MarkJosephRemetio</div>
+            <div>It is what it is.</div>
+            <div><a href="https://www.facebook.com/markjoseph.remetio">https://www.facebook.com/markjoseph.remetio</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+    
+        <div class="col-4"><img class="w-100" decoding="auto" sizes="313.734375px" srcset="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=20fb33bf7f98eea0adb94505bffe9017&amp;oe=5EDCF2E9 640w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=b13613598565cca54abc7987e7e6ca4d&amp;oe=5EDDD6E9 750w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E 1080w" src="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E" style="object-fit: cover;"></div>
+        <div class="col-4"><img class="w-100" decoding="auto" sizes="313.734375px" srcset="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=20fb33bf7f98eea0adb94505bffe9017&amp;oe=5EDCF2E9 640w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=b13613598565cca54abc7987e7e6ca4d&amp;oe=5EDDD6E9 750w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E 1080w" src="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E" style="object-fit: cover;"></div>
+        <div class="col-4"><img class="w-100 " decoding="auto" sizes="313.734375px" srcset="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=20fb33bf7f98eea0adb94505bffe9017&amp;oe=5EDCF2E9 640w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=b13613598565cca54abc7987e7e6ca4d&amp;oe=5EDDD6E9 750w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E 1080w" src="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E" style="object-fit: cover;"></div>
+
+    </div>
+</div>
+@endsection
