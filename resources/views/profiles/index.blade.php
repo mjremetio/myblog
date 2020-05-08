6efeac9b578f8e90381d0ed09a24620e5f9f9f05
mjremetio <mj.remetio001@gmail.com>
2020-05-08 08:09:03 +0800

    able to add post and view
    able to add post and view

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,10 +9,10 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between">
                 <h1 class="h1">{{ $user->username }}</h1>
-                <a href="#">Add New Post</a>
+                <a href="/p/create ">Add New Post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>50</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>1k</strong> followers</div>
                 <div class="pr-5"><strong>3k</strong> following</div>
             </div>
@@ -21,11 +21,14 @@
             <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
-    <div class="row pt-4">
-    
-        <div class="col-4"><img class="w-100" decoding="auto" sizes="313.734375px" srcset="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=20fb33bf7f98eea0adb94505bffe9017&amp;oe=5EDCF2E9 640w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=b13613598565cca54abc7987e7e6ca4d&amp;oe=5EDDD6E9 750w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E 1080w" src="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E" style="object-fit: cover;"></div>
-        <div class="col-4"><img class="w-100" decoding="auto" sizes="313.734375px" srcset="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=20fb33bf7f98eea0adb94505bffe9017&amp;oe=5EDCF2E9 640w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=b13613598565cca54abc7987e7e6ca4d&amp;oe=5EDDD6E9 750w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E 1080w" src="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E" style="object-fit: cover;"></div>
-        <div class="col-4"><img class="w-100 " decoding="auto" sizes="313.734375px" srcset="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=20fb33bf7f98eea0adb94505bffe9017&amp;oe=5EDCF2E9 640w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=b13613598565cca54abc7987e7e6ca4d&amp;oe=5EDDD6E9 750w,https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E 1080w" src="https://instagram.fmnl4-6.fna.fbcdn.net/v/t51.2885-15/e35/60883181_860127004355071_4985141630559242859_n.jpg?_nc_ht=instagram.fmnl4-6.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=5Whnp_gOwtsAX-zF8R9&amp;oh=9a7c7aa7283ae9b60b711581bcffc3b2&amp;oe=5EDDA10E" style="object-fit: cover;"></div>
+    <div class="row pt-5">
+        @foreach($user->posts as $post)
+            <div class="col-4">
+                <a href="/p/{{ $post->id }}">
+                    <img class="w-100 h-100 pb-4"  src="/storage/{{ $post->image }}" style="object-fit: cover;">
+                </a>
+            </div>
+        @endforeach
 
     </div>
 </div>
