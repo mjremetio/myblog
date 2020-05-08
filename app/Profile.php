039b5555908be4dbb622665cd0d4f0fca15e823b
mjremetio <mj.remetio001@gmail.com>
2020-05-08 12:50:28 +0800

    followers and following user functionality
    followers and following user functionality

--- app/Profile.php
+++ app/Profile.php
@@ -7,6 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     //
+    protected $guarded = [];
+
+    public function profileImage(){
+        $imagePath = ($this->image) ? $this->image : 'profile/no_image.png';
+        return '/storage/' . $imagePath ;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,17 +4,26 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-19/s150x150/57348348_279964586285572_6163317076652982272_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_ohc=Sw597m3fxiQAX9a6sup&oh=6953db302404296d19db7db73137ae52&oe=5EDBC8E3" alt="no image detected" class="rounded-circle">
+            <img src="{{ $user->profile->profileImage() }}" alt="no image detected" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
-            <div class="d-flex justify-content-between">
-                <h1 class="h1">{{ $user->username }}</h1>
-                <a href="/p/create ">Add New Post</a>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">{{ $user->username }}</div>
+                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
+                </div>
+                @can('update', $user->profile)
+                    <a href="/p/create ">Add New Post</a>
+                @endcan
+         
             </div>
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
-                <div class="pr-5"><strong>1k</strong> followers</div>
-                <div class="pr-5"><strong>3k</strong> following</div>
+                <div class="pr-5"><strong>{{ $user->profile->followers->count() }}</strong> followers</div>
+                <div class="pr-5"><strong>{{ $user->following->count() }}</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
             <div>{{ $user->profile->description }}</div>
