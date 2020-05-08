4ba8c145bbe1c827c8d963be02d8be42de814737
mjremetio <mj.remetio001@gmail.com>
2020-05-08 18:05:09 +0800

    final commit
    final

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
@@ -12,28 +11,9 @@ class ProfilesController extends Controller
     public function index(User $user)
     {
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
-        
-        $postCount = Cache::remember(
-            'count.posts' . $user->id, 
-            now()->addSeconds(30), 
-            function() use($user) {
-                return $user->posts->count();
-            });
-        $followersCount = Cache::remember(
-            'count.posts' . $user->id, 
-            now()->addSeconds(30), 
-            function() use($user) {
-                return $user->profile->followers->count();
-            });
-        $followingCount = Cache::remember(
-            'count.posts' . $user->id, 
-            now()->addSeconds(30), 
-            function() use($user) {
-                return $user->following->count();
-            });
 
-      
-        return view('profiles.index', compact('user', 'follows', 'postCount', 'followersCount', 'followingCount'));
+       
+        return view('profiles.index', compact('user', 'follows'));
     }
     public function edit(User $user)
     {
