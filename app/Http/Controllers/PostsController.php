55cc153bdb6f1ec01c270cf0bf5d6e5b08f59054
mjremetio <mj.remetio001@gmail.com>
2020-05-08 14:05:12 +0800

    load the index posts and add telescope plugin
    load the index posts and add telescope plugin

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Post;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 
@@ -12,6 +12,17 @@ class PostsController extends Controller
     {
         $this->middleware('auth');
     }
+
+    public function index()
+    {
+        $users = auth()->user()->following()->pluck('profiles.user_id');
+        
+        $posts = Post::whereIn('user_id', $users)->with('user')->latest()->paginate(5);
+
+        return view('posts.index', compact('posts'));
+
+    }
+
     public function create()
     {
         return view('posts.create');
@@ -39,6 +50,7 @@ class PostsController extends Controller
     
          return redirect('/profile/' . auth()->user()->id);
     }
+    
     public function show(\App\Post $post)
     {
         return view('posts.show',compact('post'));
